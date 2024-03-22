fb75ce54c13ce03b7a5804352494a3f202504716
Angel Vargas <angel2311v@gmail.com>
2024-03-22 03:22:01 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(cards) {
+    let result = [];
+    for (let i = 0; i < cards.length; i++) {
+      result.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return result;
+  }
+  const names = ["Charlie", "Samip", "Ali"];
+const occasion = "surprise";
+const cards = writeCards(names);
+console.log(cards);
+function countDown() {
+    let number = 10
+while (number >= 0) {
+    console.log(number--);
+}
+}
+
